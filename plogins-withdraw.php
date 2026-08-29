d4911f3fa9b2aa76943c447f2b8f1df532d04415
wppoland <szatkowski.mariusz+github@gmail.com>
2026-08-29 10:33:37 +0200

    chore: declare WooCommerce 11.0 compatibility

--- plogins-withdraw.php
+++ plogins-withdraw.php
@@ -19,7 +19,7 @@ declare(strict_types=1);
  * Requires Plugins:  woocommerce
  *
  * WC requires at least: 8.0
- * WC tested up to:      10.9
+ * WC tested up to:      11.0
  */
 
 namespace Withdraw;
